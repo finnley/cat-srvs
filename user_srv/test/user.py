32ddc72f67b40a3332a331b46256daeacf2dcfdb
finnley <finnley.yuan@jingdigital.com>
2021-03-13 23:45:59 +0800

    api user

--- user_srv/test/user.py
+++ user_srv/test/user.py
@@ -12,10 +12,30 @@ class UserTest:
     def user_list(self):
         rsp: user_pb2.UserListResponse = self.stub.GetUserList(user_pb2.PageInfo(page=2, pageSize=2))
         print(rsp.total)
+
         for user in rsp.data:
             print(user.mobile, user.birthday)
+
+    def get_user_by_id(self, id):
+        rsp: user_pb2.UserInfoResponse = self.stub.GetUserById(user_pb2.IdRequest(id=id))
+        print(rsp.mobile)
+
+    def get_user_by_mobile(self, mobile):
+        rsp: user_pb2.UserInfoResponse = self.stub.GetUserByMobile(user_pb2.MobileRequest(mobile=mobile))
+        print(rsp.mobile)
+
+    def create_user(self, nickname, mobile, password):
+        rsp: user_pb2.UserInfoResponse = self.stub.CreateUser(user_pb2.CreateUserInfo(
+            nickname=nickname,
+            password=password,
+            mobile=mobile
+        ))
+        print(rsp.id)
 
 
 if __name__ == "__main__":
     user = UserTest()
     user.user_list()
+    user.get_user_by_id(5)
+    user.get_user_by_mobile("13312345672")
+    user.create_user("4343", "14343434343", "123456")
